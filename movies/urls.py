8e11cd4dd31f6615da1208f70e360e3220bca820
Dawid <dawdom34@wp.pl>
2023-04-24 13:03:49 +0200

    Added comments to urls files

--- movies/urls.py
+++ movies/urls.py
@@ -12,13 +12,22 @@ from .views import (create_movie_view,
 app_name = 'movies'
 
 urlpatterns = [
+    # Create new movie
     path('add_movie/', create_movie_view, name='create_movie'),
+    # Create new movie session
     path('add_program/', create_program_view, name='create_program'),
+    # Filter movies by date
     path('<str:date>', date_filter_view, name='filter'),
+    # Seance details
     path('details/<int:seance_id>', movie_details_view, name='details'),
+    # Buy ticket
     path('ticket/<int:seance_id>', buy_ticket_view, name='buy_ticket'),
+    # Create ticket
     path('ticket/create', create_ticket_view, name='create_ticket'),
+    # Users tickets
     path('my_tickets/', tickets_view, name='tickets'),
+    # Return ticket
     path('ticket/return', return_ticket_view, name='return_ticket'),
+    # Load ticket QR code
     path('ticket/<int:ticket_id>/qr_code', load_qr_code_view, name='qr_code')
 ]
--- movies_api/urls.py
+++ movies_api/urls.py
@@ -11,12 +11,20 @@ from .views import( HomePageAPIView,
 app_name = 'movies_api'
 
 urlpatterns = [
+    # Home page (movie sessions for today's date)
     path('home/', HomePageAPIView.as_view(),  name='home'),
+    # Filter movies by date
     path('date_filter/', DateFilterAPIView.as_view(), name='date_filter'),
+    # Seance details
     path('details/<int:seance_id>/', MovieDetailsAPIView.as_view(), name='details'),
+    # Buy ticket
     path('ticket/<int:seance_id>/',  BuyTicketAPIView.as_view(), name='buy_ticket'),
+    # Create ticket
     path('ticket/create/', CreateTicketAPIView.as_view(), name='create_ticket'),
+    # User tickets
     path('my_tickets/', TicketsAPIView.as_view(), name='my_tickets'),
+    # Return Ticket
     path('ticket/return/', ReturnTicketAPIVIew.as_view(), name='return_ticket'),
+    # Load ticket QR code
     path('ticket/<int:ticket_id>/qr_code/', LoadQRCodeAPIView.as_view(), name='qr_code')
 ]
